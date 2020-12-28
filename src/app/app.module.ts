74d6b8bc91da75e50bbf72018ac0a0200e834128
Nightglowie <nightglowie@gmail.com>
2020-12-28 16:23:26 +0100

    buttons and pages enrollment and authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { EnrollmentComponent } from './enrollment/enrollment.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EnrollmentComponent,
+    AuthenticationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: 'enroll', component: EnrollmentComponent},
+      {path: 'authen', component: AuthenticationComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
